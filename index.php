afb2a51c0ace6aa805c1da3f26dfdc590b66848e
Crewdz <chris92501@yahoo.com>
2018-02-02 21:10:16 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+ <html>
+    <head>
+	<title>FAQ</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<link rel="stylesheet" type="text/css" href="css/bulma.css">
+	<link rel="stylesheet" type="text/css" href="css/fontawesome-all.min.css">
+	</head>
+        
+		<body>
+	<h1 id="title"><i class="fas fa-question-circle"></i>&nbsp;Frequently Asked Questions</h1>
+	<div class="columns">
+  <div class="column">
+
+  <p class="pay"><i class="fas fa-credit-card"></i>&nbsp;Payments</p>
+  <section class="list">
+  <ul id="fullwidth">
+  <li><i class="fas fa-ban"></i>&nbsp; How to cancel payments</li>
+  <li id="light"><i class="fas fa-question"></i>&nbsp; How do I pay?</li>
+  </ul>
+  </section>
+  
+</div>
+	
+	
+
+  <div class="column">
+
+  <p class="managementpanel"><i class="fas fa-columns"></i>&nbsp;Control Panel</p>
+  <section class="list">
+  <ul id="fullwidth">
+  <li><i class="fas fa-play"></i>&nbsp; How to start/stop/refresh server</li>
+  <li id="light"><i class="fas fa-cogs"></i>&nbsp; Configuration analysis</li>
+  <li><i class="fas fa-user-plus"></i>&nbsp;How to add users to authentication list</li>
+  </ul>
+  </section>
+</div>
+
+</div>
+
+	<footer class="footer">
+  <div class="container">
+    <div class="content has-text-centered">
+      <p>
+        <strong>FAQ</strong> by <a href="https://cover6.net/">Cover6</a> Copyright © 2018 Cover6. All Rghts Reserverd.
+      </p>
+    </div>
+  </div>
+</footer>
+	</body>
+	
+	
+</html>
